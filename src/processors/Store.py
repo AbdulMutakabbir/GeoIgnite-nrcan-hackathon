fbdce0bac0a9c5d6ac77e5a9931351a3844b7ed4
AbdulMutakabbir <a_mutakabbir@yahoo.com>
2025-04-23 11:15:39 -0400

    added al prove env val

--- src/processors/Store.py
+++ src/processors/Store.py
@@ -1,18 +1,27 @@
 from .ProvFuelTypeLandAreaDataProcessor import ProvFuelTypeLandAreaDataProcessor
 from .AfterFireFuelTypeGrowthProcessor import AfterFireFuelTypeGrowthProcessor
+import os
+from dotenv import load_dotenv
 
 class Store:
     def __init__(self):
+        # load env
+        load_dotenv(".env")
+
+        self.all_prov_val = os.getenv("ALL_PROV_DATA_VAL")
+
         self.__prov_processor = ProvFuelTypeLandAreaDataProcessor()
         self.__fire_processor = AfterFireFuelTypeGrowthProcessor()
 
         list_prov = self.__prov_processor.get_provs()
-        list_prov = ["Canada"] + list_prov
+        list_prov = [self.all_prov_val] + list_prov
 
         self.state = {
             "prov_fuel_type_data": self.__prov_processor.get_data(),
             "after_fire_growth_data": self.__fire_processor.get_data(),
             "prov_list": list_prov,
-            "active_prov": list_prov[0],
+            "active_prov": self.all_prov_val,
         }
+
+        del list_prov
 
