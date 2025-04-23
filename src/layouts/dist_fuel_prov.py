ff15dfb31596001320563fdc2abf61998826702e
AbdulMutakabbir <a_mutakabbir@yahoo.com>
2025-04-23 11:52:55 -0400

    code cleaning

--- src/layouts/dist_fuel_prov.py
+++ src/layouts/dist_fuel_prov.py
@@ -22,26 +22,7 @@ def get_layout(
         hovertemplate="%{label}-> %{value:,.2f}km^2 of the total land area <extra></extra>",
     )
     fig.update_layout(margin=dict(t=40, b=0, l=0, r=0))
-    # fig = px.pie(
-    #     data_frame = data_df,
-    #     names = config.DATA_DICT['label'], 
-    #     values = config.DATA_DICT['value'], 
-    #     title = config.CHART_DICT['title'],
-    #     hole = config.CHART_DICT['hole'],
-    #     color_discrete_sequence=px.colors.sequential.Reds_r,
-    #     width = config.CHART_DICT['width'],  
-    #     height = config.CHART_DICT['height'],
-    #     # sort = False,
-    # )
-    # fig.update_traces(
-    #     textposition = 'inside', 
-    #     hoverinfo = 'percent+label',
-    #     textinfo = 'percent+label',
-    #     hovertemplate="%{label}: %{percent} of the total fuel type land area <extra></extra>",
-    #     showlegend = False,
-    # )
     return html.Div([
-        # html.H1(config.CHART_DICT['title']),
         dcc.Graph(
             id = config.ID,
             figure = fig
